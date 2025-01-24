faa12c2ae764da71571a41bf8c57e99394fc7efd
Gomitag-dev <tomas.quintanaramos@gmail.com>
2025-01-24 13:14:41 -0300

    Se agrega cambios 2

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ const app = require("./app");
 const { connect } = require("./database");
 
 async function main() {
-    //DATABASE CONNECTION
+    // CONEXIÓN A LA BASE DE DATOS
     await connect();
-    //SERVER CONNECTION
-    await app.listen(3000);
-    await console.log("Server is running on port 3000:Conected");
+
+    // OBTENCIÓN DEL PUERTO DEL ENTORNO O USO DEL PUERTO 3000 COMO RESERVA
+    const PORT = process.env.PORT || 3000;
+
+    // CONEXIÓN DEL SERVIDOR
+    await app.listen(PORT);
+    console.log(`Servidor corriendo en el puerto ${PORT}: Conectado`);
 }
 
-main();
-
+main().catch(err => {
+    console.error("Error al iniciar la aplicación:", err);
+});
